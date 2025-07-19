40c370721ccd5e9da62bea77e873c755242ccc58
Muzammil Hameed <134141927+Muzammil-Hameed@users.noreply.github.com>
2025-07-19 18:54:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def predict():
         return f"An error occurred: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=False)  # Set debug=False for stable deployment
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
